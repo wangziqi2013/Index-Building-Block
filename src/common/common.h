25ade650f66136f09f357942e648bdcdef0d68df
Ziqi Wang <675973885@qq.com>
2017-09-04 15:51:31 -0400

    Adding common.h for common definitions

---
+++ src/common/common.h
@@ -0,0 +1,11 @@
+
+/*
+ * common.h - This file defines the most common environmental variables
+ *            and declarations.
+ *
+ * Since this file is included in every source files, please be cautious when
+ * changing this file. Your change may have unintended affect if not treated 
+ * seriously. Please obey the following rules when you add things into this
+ * file:
+ *
+ */

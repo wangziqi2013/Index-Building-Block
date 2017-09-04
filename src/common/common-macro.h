f5a5ca65b80ecb5925dc164d4b159392794daa06
Ziqi Wang <675973885@qq.com>
2017-09-04 16:21:22 -0400

    Adding the definition of the optional wangziqi2013 namespace

--- src/common/common-macro.h
+++ src/common/common-macro.h
@@ -6,3 +6,20 @@
  * Domain-specific macros should be defined in their own domain, and then
  * included by adding the path to compiler
  */
+
+#include "common.h"
+
+// Whether to use "wangziqi2013" name space to avoid name collision
+// with other libraries
+#define USE_WANGZIQI2013_NAMESPACE
+
+// This should be used with the ending macro as a pair
+// If the name space is not used then we just define an empty pair
+#ifdef USE_WANGZIQI2013_NAMESPACE
+  #define OPTIONAL_NAMESPACE_WANGZIQI2013 namespace wangziqi2013 {
+  #define NAMESPACE_WANGZIQI2013_END }
+#else
+  #define OPTIONAL_NAMESPACE_WANGZIQI2013
+  #define NAMESPACE_WANGZIQI2013_END
+#endif
+

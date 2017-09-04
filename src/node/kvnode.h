7907f1962eab71e5eed551aebc56f58a1173ff4a
Ziqi Wang <675973885@qq.com>
2017-09-04 15:47:49 -0400

    Adding content into kvnode.h

---
+++ src/node/kvnode.h
@@ -0,0 +1,15 @@
+
+/*
+ * kvnode.h - This file implements the most general form of fix-sized
+ *            key-value container
+ */
+
+#include "common-macro.h"
+
+namespace wangziqi2013 {
+namespace index_building_block {
+
+
+
+} // namespace index_building_block
+} // namespace wangziqi2013

dd222857331627258b1db6b140e3e7636ef59a67
Ziqi Wang <675973885@qq.com>
2017-09-04 18:13:32 -0400

    Adding common test

---
+++ test/test-common.cpp
@@ -0,0 +1,17 @@
+
+#include "common.h"
+
+/*
+ * TestDebugPrint() - This function tests whether debug printing works
+ */
+void TestDebugPrint() {
+  PrintTestName();
+  test_printf("This is a test printf\n");
+
+  return;
+}
+
+int main() {
+  TestDebugPrint();
+  return 0;
+}

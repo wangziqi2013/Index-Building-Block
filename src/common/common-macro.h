97ba007ca4f7e66fd901ea75ab270ad050c63026
Ziqi Wang <675973885@qq.com>
2017-09-04 15:51:43 -0400

    Modify macro

---
+++ src/common/common-macro.h
@@ -0,0 +1,8 @@
+/*
+ * common-macro.h - This file implements common macro definitions, usually
+ *                  at a high level, which controls compilation or chooses
+ *                  environments, etc.
+ *
+ * Domain-specific macros should be defined in their own domain, and then
+ * included by adding the path to compiler
+ */

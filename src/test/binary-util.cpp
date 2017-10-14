6759e5bac4e5b3bf67ffb4e732ce1606c6fba867
Ziqi Wang <ziqiw@mit.edu>
2017-10-14 07:09:23 -0400

    Adding Make()

--- src/test/binary-util.cpp
+++ src/test/binary-util.cpp
@@ -1,2 +1,26 @@
 
 #include "binary-util.h"
+
+/*
+ * Make() - Get a sequence of certain size. If there is any prior data, they
+ *          will be freed
+ * 
+ * The return storage is guaranteed to be zeroed out
+ */
+void BitSequence::Make(size_t new_size) {
+  // If size is 0, just err and exit
+  always_assert(new_size != 0UL);
+  if(data_p != nullptr) {
+    delete[] data_p;
+  }
+
+  size_t new_byte_size = (new_size + 7) / 8;
+
+  data_p = new uint8_t[new_byte_size];
+  memset(data_p, 0x00, new_byte_size);
+
+  // It is the bit length
+  length = new_size;
+
+  return;
+}

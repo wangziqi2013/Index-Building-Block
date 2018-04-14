99c34af819aa9c7d3049ff0de58166da007d3b6e
Ziqi Wang <ziqiw@mit.edu>
2018-04-13 22:26:47 -0400

    Adding bwtree.h

---
+++ src/bwtree/bwtree.h
@@ -0,0 +1,17 @@
+
+/*
+ * bwtree.h - This file implements the BwTree
+ */
+
+#pragma once
+#ifndef _BWTREE_H
+#define _BWTREE_H
+
+#include "common.h"
+
+
+
+} // namespace index_building_block
+} // namespace wangziqi2013
+
+#endif

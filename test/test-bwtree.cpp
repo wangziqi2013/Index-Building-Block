4fc57f6fbd2fd035dcc892b82f77c76397b3302f
Ziqi Wang <ziqiw@mit.edu>
2018-04-13 22:56:21 -0400

    Adding test bwtree

---
+++ test/test-bwtree.cpp
@@ -0,0 +1,22 @@
+
+#include "bwtree/bwtree.h"
+
+using namespace wangziqi2013;
+using namespace index_building_block;
+
+/*
+ * MappingTableTest() - Tests mapping table
+ * 
+ * 1. Single thread allocation of node ID
+ * 2. Concurrent allocation of node ID
+ * 3. CAS
+ * 4. Overflow under debug mode
+ */
+void MappingTableTest() {
+  return;
+}
+
+int main() {
+  MappingTableTest();
+  return 0;
+}

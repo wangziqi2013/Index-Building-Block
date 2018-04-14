c4271d22eceb5d223198ce95fe6d2a8254b11ba3
Ziqi Wang <ziqiw@mit.edu>
2018-04-13 22:45:46 -0400

    Adding relase node id

--- src/bwtree/bwtree.h
+++ src/bwtree/bwtree.h
@@ -17,16 +17,52 @@
  * 1. Release of node ID is not supported. Always allocate from the counter
  * 2. The mapping table is fixed sized. No bounds checking is performed under
  *    release mode. Under debug mode an error will be raised
+ * 
+ * It accepts two template parameters: One to specify the element type. The atomic
+ * type to the pointer of the element type is stored. Another to specify the 
+ * size of the mapping table, which is the number of elements
  */
-template <typedef BaseType, uint64_t TABLE_SIZE>
+template <typedef BaseNodeType, size_t TABLE_SIZE>
 class DefaultMappingTable {
  public:
+  // External class should def this
+  using NodeIDType = uint64_t;
+
+  /*
+   * AllocateNodeID() - Allocate a slot and put the given node_p into it
+   * 
+   * If allocation fails because of table overflow, an error is raised under 
+   * debug mode. The slot is always returned even if node_p is given
+   */
+  inline NodeIDType AllocateNodeID(BaseNodeType *node_p) {
+    // Use atomic instruction to allocate slots
+    NodeIDType slot = next_slot.fetch_and_add(1);
+    // Only do this after the atomic inc
+    assert(slot < TABLE_SIZE);
+    mapping_table[slot] = node_p;
+
+    return slot;
+  }
+
+  /*
+   * ReleaseNodeID() - Release the node ID
+   * 
+   * The minimal implementation does not support this. Just leak the node ID.
+   * Subclasses could extend this class by overriding this function to provide
+   * node ID release capabilities.
+   */
+  inline void ReleaseNodeID(NodeIDType node_id) {
+    assert(node_id < TABLE_SIZE);
+    (void)node_id; 
+    return;
+  }
+
   
 
  private:
   // Fixed sized mapping table with atomic type as elements
-  atomic<BaseType> mapping_table[TABLE_SIZE];
-  uint64_t next_slot;
+  std::atomic<BaseNodeType *> mapping_table[TABLE_SIZE];
+  std::atomic<NodeIDType> next_slot;
 };
 
 } // namespace index_building_block

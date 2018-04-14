ef6bcb9e33bc06ab2d0e698477b4f66ee6d16194
Ziqi Wang <ziqiw@mit.edu>
2018-04-13 22:30:53 -0400

    Working on default mapping table

--- src/bwtree/bwtree.h
+++ src/bwtree/bwtree.h
@@ -8,8 +8,26 @@
 #define _BWTREE_H
 
 #include "common.h"
+#include <atomic>
 
+/*
+ * class DefaultMappingTable - This class implements the minimal mapping table
+ *                             which supports the allocation and CAS of node IDs
+ * 
+ * 1. Release of node ID is not supported. Always allocate from the counter
+ * 2. The mapping table is fixed sized. No bounds checking is performed under
+ *    release mode. Under debug mode an error will be raised
+ */
+template <typedef BaseType, uint64_t TABLE_SIZE>
+class DefaultMappingTable {
+ public:
+  
 
+ private:
+  // Fixed sized mapping table with atomic type as elements
+  atomic<BaseType> mapping_table[TABLE_SIZE];
+  uint64_t next_slot;
+};
 
 } // namespace index_building_block
 } // namespace wangziqi2013

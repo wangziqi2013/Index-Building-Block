0ed713a46322fd904c03ec22efd4775095abd3d1
Ziqi Wang <ziqiw@mit.edu>
2018-04-20 20:32:24 -0400

    Adding bwtree.cpp

---
+++ src/bwtree/bwtree.cpp
@@ -0,0 +1,17 @@
+
+#include "bwtree.h"
+
+namespace wangziqi2013 {
+namespace index_building_block {
+namespace bwtree {
+
+template <typename KeyType>
+template <typename DeltaChainType>
+ExtendedNodeBase<KeyType, DeltaChainType> *NodeBase<KeyType>::GetBase() { 
+  return reinterpret_cast<ExtendedNodeBase<KeyType, DeltaChainType> *>(
+    reinterpret_cast<char *>(GetLowKey()) - ExtendedNodeBase<KeyType>::LOW_KEY_OFFSET); 
+}
+
+} // namespace bwtree
+} // namespace index_building_block
+} // namespace wangziqi2013
